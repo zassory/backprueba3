cee71b97910a80966f06729119a45f961cc680c9
Nicolas <nicolas.programador@gmail.com>
2022-11-15 12:32:19 -0300

    Crud listo, falta refactorizar y modular

--- routes/cursos.js
+++ routes/cursos.js
@@ -1,12 +1,123 @@
 const express = require('express');
+const Curso = require('../models/curso_model');
 const { response } = express;
 const ruta = express.Router();
 
 ruta.get('/', (req,res) => {
-    res.status(200).json({
-        ok:200,
-        msg:'Listo el GET de cursos'
+    let resultado = listarCursosActivos();
+    resultado.then( curso => {
+        res.json({
+            curso
+        })
+    }).catch( err => {
+        res.status(400).json({
+            err
+        });
     });
+});
+
+ruta.post('/',(req,res=response) => {
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+ruta.delete('/:id', (req,res = response) => {
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err);
+    })
 })
+
+ruta.put('/:id', (req,res=response) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    let resultado = actualizarCurso( req.params.id , req.body );    
+    resultado.then(curso => {
+         res.json({
+             curso
+         })
+     }).catch(err => {
+         res.status(400).json({
+             err
+         })
+     })
+})
+
+ruta.patch('/:id', (req,res = response) => {
+    
+     let resultado = activarCurso(req.params.id);
+     resultado.then( curso => {
+         res.json({
+             curso
+         })
+     }).catch(err => {
+         res.status(400).json({
+             err
+         })
+     })
+})
+
+ruta.delete('/:id', (req,res=response) => {
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then( curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+
+
+const crearCurso = async(body) => {
+    let curso = new Curso({
+        titulo : body.titulo,
+        descripcion : body.desc
+    });
+    return await curso.save();
+}
+
+const actualizarCurso = async(id,body) => {
+    let curso = await Curso.findByIdAndUpdate({"_id":id}, {
+        $set : {
+            titulo: body.titulo,
+            descripcion: body.desc
+        }
+    },{new:true});
+    return curso;
+}
+
+const desactivarCurso = async(id) => {
+    let curso = await Curso.findByIdAndUpdate({"_id":id},{
+        $set: {
+            estado: false
+        }
+    },{new:true});
+    return curso;
+}
+
+const activarCurso = async(id) => {
+    let curso = await Curso.findByIdAndUpdate({"_id":id}, {
+        $set : {
+            estado: true
+        }
+    },{new:true});
+    return curso;
+}
+
+const listarCursosActivos = async() => {
+    let cursos = await Curso.find({"estado": true});
+    return cursos;
+}
 
 module.exports = ruta;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const { response } = express;
 const Usuario = require('../models/usuario_model');
+const { response } = express;
+const Joi = require('joi');
 const ruta = express.Router();
+
+const schema = Joi.object({
+    nombre: Joi.string()        
+        .min(3)
+        .max(30)
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+});
 
 ruta.get('/',(req,res = response) => {
 
-    let resultado = listarUsuarios();
+    let resultado = listarUsuariosActivos();
     resultado.then( users => {
-        res.status(200).json({
-            ok:true,
-            users
-        })
+        res.json(users)
     }).catch( err => {
-        res.status(400).json({
-            ok:false,
+        res.status(400).json({            
             error:err
         })
     });    
@@ -23,47 +33,69 @@ ruta.get('/',(req,res = response) => {
 
 ruta.post('/', (req,res = response) => {
     let body = req.body;
-    let resultado = crearUsuario(body);
 
-    resultado.then( user => {
-        res.json({            
-            valor: user
-        })
-    }).catch( err => {
+    const { error, value } = schema.validate({nombre:body.nombre,email:body.email});
+    if(!error){
+        let resultado = crearUsuario(body);
+
+        resultado.then( user => {
+            res.json({            
+                valor: user
+            })
+        }).catch( err => {
+            res.status(400).json({
+                error:err
+            })
+        });
+    }else{
         res.status(400).json({
-            error:err
+            error
         })
-    });
+    }
+
+    
 });
 
 ruta.put('/:email', (req,res=response) => {
-    let resultado = actualizarUsuario( req.params.email , req.body );    
-     resultado.then( valor => {
-          res.json({
-              valor:valor
-          })
-      }).catch( err => {
-          res.status(400).json({
-              error:err
-          })
-      });
+
+    const { error, value } = schema.validate({nombre:req.body.nombre});
+
+    if(!error){
+        let resultado = actualizarUsuario( req.params.email , req.body );    
+        resultado.then( valor => {
+            res.json({
+                valor
+            })
+        }).catch( err => {
+            res.status(400).json({
+                err
+            })
+        });
+    }else{
+        res.status(400).json({
+            error
+        });
+    }
+
+    
 });
 
 ruta.delete('/:email', (req,res = response) => {
     let resultado = desactivarUsuario(req.params.email);
     resultado.then( valor => {
         res.json({
-            usuario: valor
+            valor
         })
     }).catch(err => {
         res.status(400).json({
-            error: err
+            err
         })
     })
 });
 
-const listarUsuarios = async() => {
-    return await Usuario.find();    
+const listarUsuariosActivos = async() => {
+    let usuarioActivo =  await Usuario.find({"estado":true});
+    return usuarioActivo;
 }
 //Una funcion asyncrona nos devuelve una promesa
 const crearUsuario = async( body ) => {
@@ -76,7 +108,7 @@ const crearUsuario = async( body ) => {
 }
 
 const actualizarUsuario = async(email,body) => {
-    let usuario = await Usuario.findOneAndUpdate(email , {
+    let usuario = await Usuario.findOneAndUpdate({"email":email} , {
         $set: {
             nombre  : body.nombre,
             password: body.password
@@ -87,7 +119,7 @@ const actualizarUsuario = async(email,body) => {
 }
 
 const desactivarUsuario = async(email) => {
-    let usuario = await Usuario.findOneAndUpdate( email, {
+    let usuario = await Usuario.findOneAndUpdate( {"email":email}, {
         $set: {
             estado: false
         }
